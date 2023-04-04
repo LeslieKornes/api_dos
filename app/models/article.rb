ed22aacfe6055ced1a17b8f46e710e8d8ed7af84
Leslie Kornes <lesliekornes@gmail.com>
2023-04-04 15:32:40 -0400

    add article validations and specs

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  validates_presence_of :title, :content
+  validates :slug, presence: true, uniqueness: true
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # #validations bc testing on the class level
+  describe '#validations' do
+    let(:article) { build(:article) }
+    
+
+    it 'tests that factory is valid' do
+      # create saves obj to db. often we don't need all that
+      # article = create(:article)
+      expect(article).to be_valid
+    end
+  
+    it 'validates presence of title' do
+      article.title = ''
+      expect(article).not_to be_valid
+    end
+
+    it 'validates presence of content' do
+      article.content = ''
+      expect(article).not_to be_valid
+    end
+
+    it 'validates presence of slug' do
+      article.slug = ''
+      expect(article).not_to be_valid
+    end
+
+    it 'validates the uniqueness of the slug' do
+      article1 = create(:article)
+      expect(article1).to be_valid
+      article2 = build(:article, slug: article1.slug)
+      expect(article2).not_to be_valid
+    end
+  end
+  
 end
