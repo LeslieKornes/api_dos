133e044c26082cddc872eab5ffa7022a7faf2091
Leslie Kornes <lesliekornes@gmail.com>
2023-04-06 13:32:54 -0400

    start routing spec. still red.

---
+++ spec/routing/articles_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+# this spec doesn't care about response, status, or format
+# will only check if a route is _defined_
+RSpec.describe '/articles routes' do
+  it 'routes to articles#index' do
+    aggregate_failures do 
+      # expect(get '/articles').to route_to(controller: 'articles', action: 'index) (long-hand)
+      expect(get '/articles').to route_to('articles#index')
+      expect(get '/articles?page[number]=3').to route_to('articles#index', page: { 'number': 3 })
+    end
+  end
+end
