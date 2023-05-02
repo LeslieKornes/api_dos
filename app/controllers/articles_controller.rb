954177b7d6fdbaa4282941e2b13e99b1e58dd170
Leslie Kornes <lesliekornes@gmail.com>
2023-05-02 10:48:27 -0400

    add jsonapi-serializer gem and some spec coverage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,12 @@
 class ArticlesController < ApplicationController
   
+  def index
+    articles = Article.all
+    # status code unecessary but explicit
+    render json: serializer.new(articles), status: :ok
+  end
+
+  def serializer
+    ArticleSerializer
+  end
 end
